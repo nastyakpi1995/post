db6c97f075b9508efd30b390672eab608340adc4
Mykhayl <mgusev@s-pro.io>
2020-04-30 11:15:18 +0300

    basic project structure setup

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>WooDoc</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
